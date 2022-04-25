793a46b2a14b3dda8fadae8ea061b44f4124e959
DENGWENFENG <95220375+DENGWENFENG@users.noreply.github.com>
2022-04-25 16:12:49 +0800

    Update wzxy-healthcheck.py

--- wzxy-healthcheck.py
+++ wzxy-healthcheck.py
@@ -42,6 +42,7 @@ class WoZaiXiaoYuanPuncher:
         self.header['Host'] = "student.wozaixiaoyuan.com"
         self.header['Content-Type'] = "application/x-www-form-urlencoded"
         self.header['JWSESSION'] = self.jwsession
+        cur_time = int(round(time.time() * 1000))
         sign_data = {
             "answers": '["0","1","1"]', # 要提交的回答选项。各个学校问题可能不同，请根据自己的抓包结果修改。
             "latitude": os.environ['WZXY_LATITUDE'],
